472f413f922b4580ec0e4123dfee19efe23c486c
MicahBear <micah.b.elm@gmail.com>
2023-03-07 13:51:04 -0800

    update project usage files/ setup react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function App(){
+    return(
+        <div className = "app-container">
+            <p>testing for fun</p>
+        </div>
+    )
+}
+export default  App
