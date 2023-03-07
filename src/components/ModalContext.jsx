50bcb1eacfa888492e3eca97e7f4cfa1fafc6e67
MicahBear <micah.b.elm@gmail.com>
2023-03-07 15:11:54 -0800

    update modal context with styling

--- src/components/ModalContext.jsx
+++ src/components/ModalContext.jsx
@@ -1,10 +1,32 @@
+const MODAL_STYLES = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#FFF',
+  borderRadius: '5%',
+  padding: '50px',
+  zIndex: 1000
+}
+const OVERLAY_STYLES = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, .7)',
+  zIndex: 1000
+}
+
 export default function ModalContent({onClose}){
     return(
-        <div className="modal">
-            <div className="modal--title">
+        <div style={OVERLAY_STYLES} className="modal">
+        <div  style ={MODAL_STYLES} className="modal-container">
+            <div  className="modal--title">
                 task form
             </div>
             <button onClick={onClose}>close me</button>
+        </div>
         </div>
     )
 }
